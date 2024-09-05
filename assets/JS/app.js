ab440c528ab06f7b42acd847274b9cb271268895
Eliel trindade <123204237+elieltrindade@users.noreply.github.com>
2024-09-05 15:05:28 -0300

    JavaScript elements were added to the search result, a document was also created with club data.

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,1 +1,37 @@
-console.log("Hello World!");
+function pesquisar() {
+    let section = document.getElementById('resultados-pesquisa');
+
+    let resultados = "";
+
+    for (let clube of Object.values(clubes)) {
+        resultados +=
+            `<div class="item-resultado">
+                <div class="item-resultado-titulo">
+                        <h2>${clube.nome}</h2>
+                        ${clube.cor}
+                        ${clube.escudo}
+                    </div>
+                    <p>
+                        ${clube.descricao}
+                        <br>
+                            <strong>Mascote:</strong> ${clube.mascote}
+                            <br>
+                            <strong>Estádio:</strong> ${clube.estadio}
+                            <br>
+                            <strong>Principais títulos:</strong>
+                        <ul>
+                            <li>Brasileirão: ${clube.titulos_brasileiro}</li>
+                            <li>Libertadores: ${clube.titulos_libertadores}</li>
+                            <li>Mundial de Clubes: ${clube.titulos_Mundial}</li>
+                        </ul>
+                        Visite o site do ${clube.nome} <a href=${clube.link} alt=${clube.link_alt} target="_blank">clicando aqui</a>.
+                    </p>
+                </div>
+            </div>`
+    }
+    section.innerHTML = resultados;
+}
+
+
+
+//section. innerHTML é um elemento crítico, para não ficar adicionando texto a cada repetiçao, é uma boa pratica coloar todo texto em uma variável e depois colocar no HTML

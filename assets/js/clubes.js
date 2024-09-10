00d03fa491515868e5bb20280eb8be62420ae9ec
Eliel trindade <123204237+elieltrindade@users.noreply.github.com>
2024-09-10 00:19:57 -0300

    update repository

---
+++ assets/js/clubes.js
@@ -0,0 +1,73 @@
+let clubes = {
+    corinthians: {
+        serie: 'a',
+        nome: 'Corinthians',
+        cor: '<span aria-label="uma bola de cor preta e outra na cor branca">⚫️⚪️</span>',
+        escudo: '<img src="assets/img/corinthians.svg" alt="Escudo do Sport Club Corinthians Paulista, com uma âncora, remos e uma bandeira com as iniciais SCP e três estrelas, em um círculo com o nome completo do clube e o ano de fundação (1910).">',
+        descricao: 'O Sport Club Corinthians Paulista, conhecido simplesmente como Corinthians, é um dos clubes de futebol mais populares do Brasil. Fundado em 15 de setembro de 1910, na cidade de São Paulo, o clube possui uma das maiores torcidas do país e uma rica história, marcada por conquistas nacionais e internacionais.',
+        mascote: 'Timão <i class="fa-solid fa-anchor" aria-label="icone de um timão"></i>',
+        estadio: 'Neo Química Arena',
+        titulos_brasileiro: '1990, 1998, 1999, 2005, 2011, 2015, 2017, 2018',
+        titulos_libertadores: '2012',
+        titulos_Mundial: '2012',
+        link: 'https://www.corinthians.com.br/',
+        link_alt: 'Site do Corinthians'
+    },
+
+    palmeiras: {
+        serie: 'a' ,
+        nome: 'Palmeiras',
+        cor: '<span aria-label="Uma bola na cor verde e outra na cor branca">🟢⚪️</span>',
+        escudo: '<img src="assets/img/Palmeiras_logo.svg" alt="Escudo do Palmeiras, um círculo verde com uma letra P branca em um escudo no centro, rodeado pela palavra Palmeiras em branco e cinco estrelas.">',
+        descricao: 'O Sociedade Esportiva Palmeiras, conhecido como Palmeiras, é um dos clubes de futebol mais tradicionais do Brasil. Fundado em 14 de agosto de 1914, na cidade de São Paulo, o clube possui uma vasta história e uma torcida apaixonada. O Palmeiras é conhecido por suas diversas conquistas e por ser um dos maiores clubes do futebol brasileiro.',
+        mascote: 'Periquito <i class="fa-solid fa-crow" aria-label="icone de uma passaro" style=color:#06aa48></i> e Porco <i class="fa-solid fa-piggy-bank" aria-label="icone de um porco" style=color:#06aa48></i>',
+        estadio: 'Allianz Parque',
+        titulos_brasileiro: '1967, 1972, 1993, 1994, 2016, 2018, 2022',
+        titulos_libertadores: '1999, 2020, 2021',
+        titulos_Mundial: null,
+        link: 'https://www.palmeiras.com.br/',
+        link_alt: 'Site do Palmeiras'
+    },
+    redBullBragantino: {
+        serie: 'a',
+        nome: 'Red Bull Bragantino',
+        cor: '<span aria-label="bola na vermelha, uma na cor branca e outra na cor azul escuro">🔴⚪️🔵</span>',
+        escudo: ' <img src="assets/img/red-bull-bragantino.svg" alt="Escudo do Red Bull Bragantino, com um escudo azul com as palavras Red Bull e Bragantino em branco, dois touros vermelhos em cada lado e uma bola de futebol amarela no centro.">',
+        descricao: ' O Red Bull Bragantino, conhecido como Bragantino, é um clube de futebol brasileiro com sede em Bragança Paulista, interior de São Paulo. Fundado em 1928, o clube passou por diversas transformações até se tornar uma das forças do futebol brasileiro nos últimos anos, com o investimento da Red Bull.',
+        mascote: 'Touro <span aria-label="emoji de touro de cor marrom">🐂',
+        estadio: 'Nabi Abi Chedid',
+        titulos_brasileiro: null,
+        titulos_libertadores: null,
+        titulos_Mundial: null,
+        link: 'https://www.redbullbragantino.com.br/',
+        link_alt: 'Site do Red Bull Bragantino'
+    },
+    saoPaulo: {
+        serie: 'a',
+        nome: 'São Paulo FC',
+        cor: ' <span aria-label="Uma bola na cor pretra, uma na cor vermelha e outra na cor branca">⚫️🔴⚪️</span>',
+        escudo: '<img src="assets/img/Brasao_do_Sao_Paulo_Futebol_Clube.svg"               alt="Escudo do São Paulo Futebol Clube, com um losango dividido em três partes: a superior com as letras SPFC em branco, a esquerda inferior na cor vermelha e a direita inferior na cor preta.">',
+        descricao: 'O São Paulo Futebol Clube, conhecido como São Paulo FC ou simplesmente São Paulo, é um dos clubes de futebol mais tradicionais do Brasil, com uma vasta torcida e uma história vitoriosa. Fundado em 25 de janeiro de 1930, na cidade de São Paulo, o clube conquistou diversos títulos nacionais e internacionais, destacando-se como um dos maiores do futebol brasileiro.',
+        mascote: 'Santo Paulo',
+        estadio: 'Morumbi',
+        titulos_brasileiro: '1977, 1991, 2008',
+        titulos_libertadores: '1992, 1993, 2005',
+        titulos_Mundial: '2005',
+        link: 'https://www.saopaulofc.net/',
+        link_alt: 'Site do São Paulo Futebol Clube'
+    },
+    santos: {
+        serie: 'b',
+        nome: 'Santos FC',
+        cor: '<span aria-label="bola na cor pretra e outra na cor branca">⚫️⚪️</span>',
+        escudo: '<img src="assets/img/Santos_logo.svg" alt="Escudo do Santos Futebol Clube com um fundo listrado verticalmente em preto e branco, contendo as letras S.F.C. em destaque e uma bola no canto superior esquerdo.">',
+        descricao: 'O Santos Futebol Clube, mais conhecido simplesmente como Santos, é um dos clubes de futebol mais tradicionais e populares do Brasil, com uma rica história e uma legião de torcedores apaixonados. Fundado em 14 de abril de 1912, na cidade de Santos, São Paulo, o clube conquistou diversos títulos nacionais e internacionais ao longo de sua trajetória, tornando-se um dos maiores representantes do futebol brasileiro no mundo.',
+        mascote: 'Peixe <span aria-label="emoji de um peixe na cor azul">🐟</span>',
+        estadio: 'Vila Belmiro',
+        titulos_brasileiro: '1961, 1962, 1963, 1964, 1965, 1968, 2002, 2004',
+        titulos_libertadores: '1962, 1963, 2011',
+        titulos_Mundial: '1962, 1963',
+        link: 'https://www.santosfc.com.br',
+        link_alt: 'Site do Santos Futebol Clube'
+    }
+};
